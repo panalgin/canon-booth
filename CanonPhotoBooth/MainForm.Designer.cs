7b731196c71e195072c3e8caa2195076d8c7269e
panalgin <mashadow@outlook.com>
2018-04-07 18:34:42 +0300

    test commit

--- CanonPhotoBooth/MainForm.Designer.cs
+++ CanonPhotoBooth/MainForm.Designer.cs
@@ -28,22 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.QuickFrame_Button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(99, 70);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(519, 363);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // QuickFrame_Button
+            // 
+            this.QuickFrame_Button.Location = new System.Drawing.Point(506, 41);
+            this.QuickFrame_Button.Name = "QuickFrame_Button";
+            this.QuickFrame_Button.Size = new System.Drawing.Size(112, 23);
+            this.QuickFrame_Button.TabIndex = 1;
+            this.QuickFrame_Button.Text = "Take A Snapshot";
+            this.QuickFrame_Button.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(630, 445);
+            this.Controls.Add(this.QuickFrame_Button);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Canon";
             this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button QuickFrame_Button;
     }
 }
 
