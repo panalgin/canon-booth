97f8a91a5d0688abcd0a4f93a1541940cebdca20
panalgin <mashadow@outlook.com>
2018-04-08 18:16:07 +0300

    First gif frames

--- CanonPhotoBooth/MainForm.Designer.cs
+++ CanonPhotoBooth/MainForm.Designer.cs
@@ -33,7 +33,12 @@
             this.label1 = new System.Windows.Forms.Label();
             this.Start_Button = new System.Windows.Forms.Button();
             this.Preview_Group = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Interval_Num = new System.Windows.Forms.NumericUpDown();
             this.Preview_Group.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Interval_Num)).BeginInit();
             this.SuspendLayout();
             // 
             // QuickFrame_Button
@@ -70,7 +75,7 @@
             this.Start_Button.Name = "Start_Button";
             this.Start_Button.Size = new System.Drawing.Size(75, 23);
             this.Start_Button.TabIndex = 4;
-            this.Start_Button.Text = "Start Device";
+            this.Start_Button.Text = "&Start";
             this.Start_Button.UseVisualStyleBackColor = true;
             this.Start_Button.Click += new System.EventHandler(this.Start_Button_Click);
             // 
@@ -79,18 +84,62 @@
             this.Preview_Group.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.Preview_Group.Controls.Add(this.pictureBox1);
             this.Preview_Group.Location = new System.Drawing.Point(16, 86);
             this.Preview_Group.Name = "Preview_Group";
-            this.Preview_Group.Size = new System.Drawing.Size(602, 347);
+            this.Preview_Group.Size = new System.Drawing.Size(358, 293);
             this.Preview_Group.TabIndex = 5;
             this.Preview_Group.TabStop = false;
             this.Preview_Group.Text = "Live Preview";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(352, 274);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 6;
+            // 
+            // Interval_Num
+            // 
+            this.Interval_Num.Location = new System.Drawing.Point(167, 43);
+            this.Interval_Num.Maximum = new decimal(new int[] {
+            20000,
+            0,
+            0,
+            0});
+            this.Interval_Num.Minimum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.Interval_Num.Name = "Interval_Num";
+            this.Interval_Num.Size = new System.Drawing.Size(120, 20);
+            this.Interval_Num.TabIndex = 7;
+            this.Interval_Num.Value = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.Interval_Num.ValueChanged += new System.EventHandler(this.Interval_Num_ValueChanged);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(630, 445);
+            this.Controls.Add(this.Interval_Num);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.Preview_Group);
             this.Controls.Add(this.Start_Button);
             this.Controls.Add(this.label1);
@@ -101,6 +150,8 @@
             this.Text = "Canon";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.Preview_Group.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Interval_Num)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -112,6 +163,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button Start_Button;
         private System.Windows.Forms.GroupBox Preview_Group;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown Interval_Num;
     }
 }
 
--- CanonPhotoBooth/MainForm.cs
+++ CanonPhotoBooth/MainForm.cs
@@ -1,15 +1,26 @@
-﻿using System;
+﻿using AForge.Video.DirectShow;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CanonPhotoBooth
 {
     public partial class MainForm : Form
     {
+        private bool DeviceExist = false;
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource = null;
+        private Timer timer = new Timer();
+        private double captureInterval = 200;
+
         public MainForm()
         {
             InitializeComponent();
@@ -17,7 +28,14 @@ namespace CanonPhotoBooth
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            GetVideoDevices();
+        }
 
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            label2.Text = "Device running... " + videoSource.FramesReceived.ToString() + " FPS";
         }
 
         private void QuickFrame_Button_Click(object sender, EventArgs e)
@@ -25,9 +43,124 @@ namespace CanonPhotoBooth
 
         }
 
+        private void CloseVideoSource()
+        {
+            if (!(videoSource == null))
+                if (videoSource.IsRunning)
+                {
+                    videoSource.SignalToStop();
+                    videoSource = null;
+                }
+        }
+
         private void Start_Button_Click(object sender, EventArgs e)
         {
+            if (this.Start_Button.Text == "&Start")
+            {
+                if (DeviceExist)
+                {
+                    videoSource = new VideoCaptureDevice(videoDevices[Cameras_Combo.SelectedIndex].MonikerString);
 
+                    var capabilities = videoSource.VideoCapabilities;
+
+                    videoSource.VideoResolution = capabilities[0];
+                    videoSource.NewFrame += VideoSource_NewFrame;
+                    videoSource.SetCameraProperty(CameraControlProperty.Exposure, -5, CameraControlFlags.Manual);
+                    videoSource.SetCameraProperty(CameraControlProperty.Focus, -5, CameraControlFlags.Auto);
+                    
+                    videoSource.ProvideSnapshots = true;
+
+                    /*AForge.Video.DirectShow.
+                    videoSource.SnapshotResolution.FrameSize = new Size 
+                       w*/ 
+                    CloseVideoSource();
+
+                    videoSource.Start();
+                    label2.Text = "Device running...";
+                    this.Start_Button.Text = "&Stop";
+                    timer.Enabled = true;
+                    timer.Start();
+                }
+                else
+                {
+                    label2.Text = "Error: No Device selected.";
+                }
+            }
+            else
+            {
+                if (videoSource.IsRunning)
+                {
+                    timer.Enabled = false;
+                    timer.Stop();
+                    CloseVideoSource();
+                    label2.Text = "Device stopped.";
+                    this.Start_Button.Text = "&Start";
+                }
+            }
+        }
+
+        ImageCodecInfo encoder = ImageCodecInfo.GetImageEncoders().First(c => c.FormatID == ImageFormat.Jpeg.Guid);
+        EncoderParameters encParams = new EncoderParameters() { Param = new[] { new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 100L) } };
+
+        Task EncodeImage(Bitmap map)
+        {
+            var stream = new MemoryStream();
+            map.Save(stream, encoder, encParams);
+
+            Image jpeg = Bitmap.FromStream(stream);
+
+            pictureBox1.BeginInvoke((MethodInvoker)delegate ()
+            {
+                pictureBox1.Image = jpeg;
+            });
+
+            return Task.FromResult(true);
+        }
+
+        DateTime lastSnapshotAt = DateTime.MinValue;
+
+        private void VideoSource_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            if (DateTime.Now - lastSnapshotAt > TimeSpan.FromMilliseconds(captureInterval))
+            {
+                lastSnapshotAt = DateTime.Now;
+
+                using (Bitmap img = (Bitmap)eventArgs.Frame.Clone())
+                {
+
+                      EncodeImage(img);
+                }
+            }
+        }
+
+        private void GetVideoDevices()
+        {
+            try
+            {
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+                if (videoDevices.Count == 0)
+                    throw new ApplicationException();
+
+                DeviceExist = true;
+
+                foreach (FilterInfo device in videoDevices)
+                {
+                    Cameras_Combo.Items.Add(device.Name);
+                }
+
+                Cameras_Combo.SelectedIndex = 0; //make dafault to first cam
+            }
+            catch (ApplicationException)
+            {
+                DeviceExist = false;
+                Cameras_Combo.Items.Add("No capture device on your system");
+            }
+        }
+
+        private void Interval_Num_ValueChanged(object sender, EventArgs e)
+        {
+            captureInterval = Convert.ToDouble(this.Interval_Num.Value);
         }
     }
 }
