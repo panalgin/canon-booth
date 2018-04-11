48551aa219f361a200cfd86decbeac517804aa46
panalgin <mashadow@outlook.com>
2018-04-11 16:10:00 +0300

    cefsharp implt

---
+++ CanonPhotoBooth/LeftScreenForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CanonPhotoBooth
+{
+    public partial class LeftScreenForm : Form
+    {
+        public LeftScreenForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
